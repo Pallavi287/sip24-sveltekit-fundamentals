c2b469c52e75a0446b96f47dea301223c4fcfff2
Pallavi287 <143804792+Pallavi287@users.noreply.github.com>
2024-07-09 21:44:52 +0530

    Task 3
    Bonus challenge insta clone

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,17 @@
-# Welcome to GitHub Desktop!
-
-This is your README. READMEs are where you can communicate what your project is and how to use it.
-
-Write your name on line 6, save it, and then head back to GitHub Desktop.
-@@ -1,19 +1,19 @@
 <script>
     export let data;
 </script>
+
 <header class="bg-white py-4 shadow-md sticky top-0 z-10">
     <div class="container mx-auto px-4 flex justify-between items-center">
         <h1 class="text-2xl font-bold font-['Comic_Sans_MS']">SlamBook</h1>
-        <a href="/add-post"class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Add post</a>
+        <a href="/add-post"class="bg-violet-700 hover:bg-violet-800 text-white font-bold py-2 px-4 rounded">Add post</a>
     </div>
 </header>
+
 <div class="container mx-auto md:my-5">
     <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
         {#each data.posts as post}
-        <div>
-            <img class="w-full" src="data:image;base64,{post.image}" alt="Post">
         <div class="border border-gray-300 rounded-lg">
             <img class="rounded-t-lg w-full" src="data:image;base64,{post.image}" alt="Post">
             <div class="text-sm py-2">
